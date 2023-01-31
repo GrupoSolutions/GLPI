fab1453dc0e1bf2287f54f5ec5279e4ea7001557
Braian Ferraz <50770601+braferraz@users.noreply.github.com>
2023-01-31 13:32:38 -0300

    Updating 10.0.4

---
+++ marketplace/gantt/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1.0',
+        'reference' => '929df187f42853ff4af3e2d4be8fb49dc10fa20b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '929df187f42853ff4af3e2d4be8fb49dc10fa20b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
